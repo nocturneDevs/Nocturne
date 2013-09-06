e99df82d2e74e065c5595c06616a772b28d10f92
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-09-06 19:34:09 -0400

    Slight beautification of website

--- index.php
+++ index.php
@@ -14,32 +14,34 @@
 	</div>
 
 	
-	<div class="bioColumn" id="bioColumnLeft">
-		<h2>Sanjay Guruprasad</h2>
-		<p>
-			Sanjay comes from a family of coders. His parents
-			started up in the late 80s - their company Softrade 
-			has now been building bullet-proof code for over 
-			25 years. Having done his undergraduate thesis in 
-			Natural Language Processing (Python/SQL) and then 
-			working at an iBank where he automated various systems, 
-			code runs in his blood. He is very into minimalist 
-			design, iOS and the web.
-		</p>
-	</div>
-	<div class="bioColumn" id="bioColumnRight">
-		<h2>Hari Mohanraj</h2>
-		<p>
-			Hari is currently pursuing his Masters degree in 
-			Music Technology at New York University. His 
-			engineering background, in tandem with his current 
-			work in music technology, gives him a comprehensive 
-			knowledge of digital signal processing, spatial audio, 
-			and coding. He has worked on several projects involving 
-			web audio, web design, 3-D audio, and auditory displays. 
-			He is an avid musician, and of course, he also loves 
-			video games.
-		</p>
+	<div id="bioWrapper">
+		<div class="bioColumn" id="bioColumnLeft">
+			<h2>Sanjay Guruprasad</h2>
+			<p>
+				Sanjay comes from a family of coders. His parents
+				started up in the late 80s - their company Softrade 
+				has now been building bullet-proof code for over 
+				25 years. Having done his undergraduate thesis in 
+				Natural Language Processing (Python/SQL) and then 
+				working at an iBank where he automated various systems, 
+				code runs in his blood. He is very into minimalist 
+				design, iOS and the web.
+			</p>
+		</div>
+		<div class="bioColumn" id="bioColumnRight">
+			<h2>Hari Mohanraj</h2>
+			<p>
+				Hari is currently pursuing his Masters degree in 
+				Music Technology at New York University. His 
+				engineering background, in tandem with his current 
+				work in music technology, gives him a comprehensive 
+				knowledge of digital signal processing, spatial audio, 
+				and coding. He has worked on several projects involving 
+				web audio, web design, 3-D audio, and auditory displays. 
+				He is an avid musician, and of course, he also loves 
+				video games.
+			</p>
+		</div>
 	</div>
 	
 <?php get_footer(); ?>
