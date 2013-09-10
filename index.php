22c262699ec25183a038c4109a1436dacda2b617
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-09-09 20:45:07 -0400

    More work - See desc.
    - Mouse hover colors now transition
    - Added email and pic placeholders in bio
    - Replaced px's with rem's \m/
    - Font size is also responsive
    - Rearranged div structure
    - Cleaned up less code
    - I should make more commits next time

--- index.php
+++ index.php
@@ -8,40 +8,45 @@
 			now - music, theatre and of course, code. Both Engineering 
 			Physics graduates with a passion for the arts, they 
 			were drawn to front end design - the perfect fusion 
-			of creativity and analytical skill. And thus Nocturne 
-			was born.
+			of creativity and analytical skill. Welcome to Nocturne.
 		</p>
 	</div>
 
 	
-	<div id="bioWrapper">
-		<div class="bioColumn" id="bioColumnLeft">
-			<h2><a>Sanjay Guruprasad</a></h2>
-			<p>
-				Sanjay comes from a family of coders. His parents
-				started up in the late 80s - their company Softrade 
-				has now been building bullet-proof code for over 
-				25 years. Having done his undergraduate thesis in 
-				Natural Language Processing (Python/SQL) and then 
-				working at an iBank where he automated various systems, 
-				code runs in his blood. He is very into minimalist 
-				design, iOS and the web.
-			</p>
+	<div class="bioColumn" id="bioColumnLeft">
+		<h2><a href="https://twitter.com/sanjaypojo" target="_blank">Sanjay Guruprasad</a></h2>
+		<p class="email"><a href="mailto:sanjay@nocturnedevs.com" target="_blank">sanjay@nocturnedevs.com</a></p>
+		<div class="imgWrapper">
+			<div class="biopic"></div>
 		</div>
-		<div class="bioColumn" id="bioColumnRight">
-			<h2><a>Hari Mohanraj</a></h2>
-			<p>
-				Hari is currently pursuing his Masters degree in 
-				Music Technology at New York University. His 
-				engineering background, in tandem with his current 
-				work in music technology, gives him a comprehensive 
-				knowledge of digital signal processing, spatial audio, 
-				and coding. He has worked on several projects involving 
-				web audio, web design, 3-D audio, and auditory displays. 
-				He is an avid musician, and of course, he also loves 
-				video games.
-			</p>
+		<p class="content">
+			Sanjay comes from a family of coders. His parents
+			started up in the late 80s - their company Softrade 
+			has now been building bullet-proof code for over 
+			25 years. Having done his undergraduate thesis in 
+			Natural Language Processing (Python/SQL) and then 
+			working at an iBank where he automated various systems, 
+			code runs in his blood. He is very into minimalist 
+			design, iOS and the web.
+		</p>
+	</div>
+	<div class="bioColumn" id="bioColumnRight">
+		<h2><a href="https://twitter.com/hari_mohanraj" target="_blank">Hari Mohanraj</a></h2>
+		<p class="email"><a href="mailto:hari@nocturnedevs.com" target="_blank">hari@nocturnedevs.com</a></p>
+		<div class="imgWrapper">
+			<div class="biopic"></div>
 		</div>
+		<p class="content">
+			Hari is currently pursuing his Masters degree in 
+			Music Technology at New York University. His 
+			engineering background, in tandem with his current 
+			work in music technology, gives him a comprehensive 
+			knowledge of digital signal processing, spatial audio, 
+			and coding. He has worked on several projects involving 
+			web audio, web design, 3-D audio, and auditory displays. 
+			He is an avid musician, and of course, he also loves 
+			video games.
+		</p>
 	</div>
 	
 <?php get_footer(); ?>
